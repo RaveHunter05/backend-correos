1dc363ee3e695da9091ded72421402ed5607bb2e
Francisco_Sotelo <francisco.sotelo@meltstudio.co>
2023-12-18 00:42:30 -0600

    feat: models for expenses and income

---
+++ Models/Expense.cs
@@ -0,0 +1,9 @@
+public class Expense
+{
+	public int ExpenseId {get; set;}
+	public string CostCenter {get; set;}
+	public string Category {get; set;}
+	public decimal ProjectedAmount {get; set;}
+	public decimal ExecutedAmount {get; set;}
+	public DateTime Date {get; set;}
+}
---
+++ Models/Income.cs
@@ -0,0 +1,9 @@
+public class Income
+{
+	public int IncomeId {get; set;}
+	public int Code {get; set;}
+	public string Service {get; set;}
+	public decimal ProjectedAmount {get; set;}
+	public decimal ExecutedAmount {get; set;}
+	public DateTime Date {get; set;}
+}
