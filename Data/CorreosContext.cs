307a59595cb849d2591b7b30c77e11a33b1c5d6d
Francisco_Sotelo <francisco.sotelo@meltstudio.co>
2023-12-18 00:43:03 -0600

    feat: migrations for income and expenses and added to the database

--- Data/CorreosContext.cs
+++ Data/CorreosContext.cs
@@ -6,4 +6,7 @@ public class CorreosContext : IdentityDbContext<ApplicationUser>
 	public CorreosContext (DbContextOptions<CorreosContext> options) : base(options)
 	{
 	}
+
+	public DbSet<Income> Incomes {get; set;}
+	public DbSet<Expense> Expenses {get; set;}
 }
---
+++ Migrations/20231218063246_IncomeExpenses.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace correos_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncomeExpenses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    ExpenseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CostCenter = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectedAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ExecutedAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.ExpenseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Incomes",
+                columns: table => new
+                {
+                    IncomeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<int>(type: "int", nullable: false),
+                    Service = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectedAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ExecutedAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incomes", x => x.IncomeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+
+            migrationBuilder.DropTable(
+                name: "Incomes");
+        }
+    }
+}
