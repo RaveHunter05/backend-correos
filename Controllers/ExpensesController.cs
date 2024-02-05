6c1e3f354a860e64a44ecd047c4eafc3e5723c7a
Francisco_Sotelo <francisco.sotelo@meltstudio.co>
2024-02-05 00:42:01 -0600

    feat: search in incomes and expenses

--- Controllers/ExpensesController.cs
+++ Controllers/ExpensesController.cs
@@ -8,115 +8,129 @@ using Microsoft.EntityFrameworkCore;
 
 namespace correos_backend.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ExpensesController : ControllerBase
-    {
-        private readonly CorreosContext _context;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ExpensesController : ControllerBase
+	{
+		private readonly CorreosContext _context;
 
-        public ExpensesController(CorreosContext context)
-        {
-            _context = context;
-        }
+		public ExpensesController(CorreosContext context)
+		{
+			_context = context;
+		}
 
-        // GET: api/Expenses
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Expense>>> GetExpenses()
-        {
-          if (_context.Expenses == null)
-          {
-              return NotFound();
-          }
-            return await _context.Expenses.ToListAsync();
-        }
+		// GET: api/Expenses
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Expense>>> GetExpenses()
+		{
+			if (_context.Expenses == null)
+			{
+				return NotFound();
+			}
+			return await _context.Expenses.ToListAsync();
+		}
 
-        // GET: api/Expenses/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Expense>> GetExpense(int id)
-        {
-          if (_context.Expenses == null)
-          {
-              return NotFound();
-          }
-            var expense = await _context.Expenses.FindAsync(id);
+		// GET: api/Expenses/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Expense>> GetExpense(int id)
+		{
+			if (_context.Expenses == null)
+			{
+				return NotFound();
+			}
+			var expense = await _context.Expenses.FindAsync(id);
 
-            if (expense == null)
-            {
-                return NotFound();
-            }
+			if (expense == null)
+			{
+				return NotFound();
+			}
 
-            return expense;
-        }
+			return expense;
+		}
 
-        // PUT: api/Expenses/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutExpense(int id, Expense expense)
-        {
-            if (id != expense.ExpenseId)
-            {
-                return BadRequest();
-            }
+		// GET: api/Expenses/search/name
+		[HttpGet("search/{costCenter}")]
+		public async Task<IEnumerable<Expense>> FindByService(string costCenter)
+		{
+			IQueryable<Expense> query = _context.Expenses;
 
-            _context.Entry(expense).State = EntityState.Modified;
+			if (!string.IsNullOrEmpty(costCenter))
+			{
+				query = query.Where(entity => entity.CostCenter!.Contains(costCenter));
+			}
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ExpenseExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			return await query.ToListAsync();
+		}
 
-            return NoContent();
-        }
+		// PUT: api/Expenses/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutExpense(int id, Expense expense)
+		{
+			if (id != expense.ExpenseId)
+			{
+				return BadRequest();
+			}
 
-        // POST: api/Expenses
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Expense>> PostExpense(Expense expense)
-        {
-          if (_context.Expenses == null)
-          {
-              return Problem("Entity set 'CorreosContext.Expenses'  is null.");
-          }
-            _context.Expenses.Add(expense);
-            await _context.SaveChangesAsync();
+			_context.Entry(expense).State = EntityState.Modified;
 
-            return CreatedAtAction("GetExpense", new { id = expense.ExpenseId }, expense);
-        }
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!ExpenseExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-        // DELETE: api/Expenses/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteExpense(int id)
-        {
-            if (_context.Expenses == null)
-            {
-                return NotFound();
-            }
-            var expense = await _context.Expenses.FindAsync(id);
-            if (expense == null)
-            {
-                return NotFound();
-            }
+			return NoContent();
+		}
 
-            _context.Expenses.Remove(expense);
-            await _context.SaveChangesAsync();
+		// POST: api/Expenses
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<Expense>> PostExpense(Expense expense)
+		{
+			if (_context.Expenses == null)
+			{
+				return Problem("Entity set 'CorreosContext.Expenses'  is null.");
+			}
+			_context.Expenses.Add(expense);
+			await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+			return CreatedAtAction("GetExpense", new { id = expense.ExpenseId }, expense);
+		}
 
-        private bool ExpenseExists(int id)
-        {
-            return (_context.Expenses?.Any(e => e.ExpenseId == id)).GetValueOrDefault();
-        }
-    }
+		// DELETE: api/Expenses/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteExpense(int id)
+		{
+			if (_context.Expenses == null)
+			{
+				return NotFound();
+			}
+			var expense = await _context.Expenses.FindAsync(id);
+			if (expense == null)
+			{
+				return NotFound();
+			}
+
+			_context.Expenses.Remove(expense);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		private bool ExpenseExists(int id)
+		{
+			return (_context.Expenses?.Any(e => e.ExpenseId == id)).GetValueOrDefault();
+		}
+	}
 }
--- Controllers/IncomesController.cs
+++ Controllers/IncomesController.cs
@@ -8,115 +8,129 @@ using Microsoft.EntityFrameworkCore;
 
 namespace correos_backend.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class IncomesController : ControllerBase
-    {
-        private readonly CorreosContext _context;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class IncomesController : ControllerBase
+	{
+		private readonly CorreosContext _context;
 
-        public IncomesController(CorreosContext context)
-        {
-            _context = context;
-        }
+		public IncomesController(CorreosContext context)
+		{
+			_context = context;
+		}
 
-        // GET: api/Incomes
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Income>>> GetIncomes()
-        {
-          if (_context.Incomes == null)
-          {
-              return NotFound();
-          }
-            return await _context.Incomes.ToListAsync();
-        }
+		// GET: api/Incomes
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Income>>> GetIncomes()
+		{
+			if (_context.Incomes == null)
+			{
+				return NotFound();
+			}
+			return await _context.Incomes.ToListAsync();
+		}
 
-        // GET: api/Incomes/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Income>> GetIncome(int id)
-        {
-          if (_context.Incomes == null)
-          {
-              return NotFound();
-          }
-            var income = await _context.Incomes.FindAsync(id);
+		// GET: api/Incomes/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Income>> GetIncome(int id)
+		{
+			if (_context.Incomes == null)
+			{
+				return NotFound();
+			}
+			var income = await _context.Incomes.FindAsync(id);
 
-            if (income == null)
-            {
-                return NotFound();
-            }
+			if (income == null)
+			{
+				return NotFound();
+			}
 
-            return income;
-        }
+			return income;
+		}
 
-        // PUT: api/Incomes/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutIncome(int id, Income income)
-        {
-            if (id != income.IncomeId)
-            {
-                return BadRequest();
-            }
+		// GET: api/Incomes/search/name
+		[HttpGet("search/{service}")]
+		public async Task<IEnumerable<Income>> FindByService(string service)
+		{
+			IQueryable<Income> query = _context.Incomes;
 
-            _context.Entry(income).State = EntityState.Modified;
+			if (!string.IsNullOrEmpty(service))
+			{
+				query = query.Where(entity => entity.Service!.Contains(service));
+			}
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!IncomeExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			return await query.ToListAsync();
+		}
 
-            return NoContent();
-        }
+		// PUT: api/Incomes/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutIncome(int id, Income income)
+		{
+			if (id != income.IncomeId)
+			{
+				return BadRequest();
+			}
 
-        // POST: api/Incomes
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Income>> PostIncome(Income income)
-        {
-          if (_context.Incomes == null)
-          {
-              return Problem("Entity set 'CorreosContext.Incomes'  is null.");
-          }
-            _context.Incomes.Add(income);
-            await _context.SaveChangesAsync();
+			_context.Entry(income).State = EntityState.Modified;
 
-            return CreatedAtAction("GetIncome", new { id = income.IncomeId }, income);
-        }
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!IncomeExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-        // DELETE: api/Incomes/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteIncome(int id)
-        {
-            if (_context.Incomes == null)
-            {
-                return NotFound();
-            }
-            var income = await _context.Incomes.FindAsync(id);
-            if (income == null)
-            {
-                return NotFound();
-            }
+			return NoContent();
+		}
 
-            _context.Incomes.Remove(income);
-            await _context.SaveChangesAsync();
+		// POST: api/Incomes
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<Income>> PostIncome(Income income)
+		{
+			if (_context.Incomes == null)
+			{
+				return Problem("Entity set 'CorreosContext.Incomes'  is null.");
+			}
+			_context.Incomes.Add(income);
+			await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+			return CreatedAtAction("GetIncome", new { id = income.IncomeId }, income);
+		}
 
-        private bool IncomeExists(int id)
-        {
-            return (_context.Incomes?.Any(e => e.IncomeId == id)).GetValueOrDefault();
-        }
-    }
+		// DELETE: api/Incomes/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteIncome(int id)
+		{
+			if (_context.Incomes == null)
+			{
+				return NotFound();
+			}
+			var income = await _context.Incomes.FindAsync(id);
+			if (income == null)
+			{
+				return NotFound();
+			}
+
+			_context.Incomes.Remove(income);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		private bool IncomeExists(int id)
+		{
+			return (_context.Incomes?.Any(e => e.IncomeId == id)).GetValueOrDefault();
+		}
+	}
 }
